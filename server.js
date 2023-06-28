50c1d1d537a854f46fca03c481410cc1bc9977ac
rotex <dantejake48@gmail.com>
2023-06-28 19:43:44 +0100

    code refactor

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import router from './routes/index';
 
-const PORT = parseInt(process.env.PORT, 10) || 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use('/', router);
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
