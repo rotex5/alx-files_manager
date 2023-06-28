e1223e5d9f4785e3054241fd9dba072975bd95d1
rotex <dantejake48@gmail.com>
2023-06-28 13:18:08 +0100

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.log(`Redis client not connected to server: ${err.message}`));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    return redisGet(key);
+  }
+
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    this.client.expire(key, time);
+  }
+
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
